1818e4c47510962713b538f62ddb8b3b8581116c
Trà Quang Duy <tranquangduy246@gmail.com>
2023-07-20 10:02:07 +0700

    Create SinglyLinkedList.java

---
+++ Skeleton-Java-1649-main/src/main/java/implementations/SinglyLinkedList.java
@@ -0,0 +1,94 @@
+package implementations;
+
+import interfaces.LinkedList;
+
+import java.util.Iterator;
+
+public class SinglyLinkedList<E> implements LinkedList<E> {
+    private Node<E> head, tail;
+    private int size;
+    private static class Node<E>{
+
+        public E element;
+        public Node<E> next;
+        public Node<E> previous;
+
+        public Node(E value) { this.element = value;
+        }
+    }
+    @Override
+    public void addFirst(E element) {
+        Node<E> newNode = new Node<>(element);
+        if (this.head == null) {
+            this.head = newNode;
+        } else {
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.size++;
+    }
+
+    @Override
+    public void addLast(E element) {
+        Node<E> newNode = new Node<>(element);
+        if (this.head == null) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.size++;
+    }
+
+    @Override
+    public E removeFirst() {
+        if (this.head == null) {
+            return null;
+        }
+        Node<E> oldNode = this.head;
+        this.head = this.head.next;
+        oldNode.next = null;
+        this.size--;
+        return oldNode.element;
+    }
+
+    @Override
+    public E removeLast() {
+        if (this.tail == null) {
+            return null;
+        }
+        Node<E> removeNode = this.head;
+        while (removeNode.next != this.tail){
+            removeNode = removeNode.next;
+        }
+        this.tail.next = null;
+        this.tail = removeNode;
+        this.size--;
+        return removeNode.next.element;
+    }
+
+    @Override
+    public E getFirst() {
+        return this.head.element;
+    }
+    @Override
+    public E getLast() {
+        return this.tail.element;
+    }
+
+    @Override
+    public int size() {
+        return this.size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+}
