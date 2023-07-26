ad936cef72c75bd4cf022eef2b5c309d6c186475
Trà Quang Duy <tranquangduy246@gmail.com>
2023-07-26 10:55:33 +0700

    Create starString.java

---
+++ Skeleton-Java-1649-main/src/main/java/implementations/starString.java
@@ -0,0 +1,16 @@
+package implementations;
+
+
+
+
+public class starString {
+    public static String starString(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException();
+        } else if (n == 0) {
+            return "*";
+        } else {
+            return starString(n - 1) + starString(n - 1);
+        }
+    }
+}
