737c92b1aef64ac9c8707a30efb3860aa650240e
Trà Quang Duy <tranquangduy246@gmail.com>
2023-08-19 08:55:24 +0700

    Create Message.java

---
+++ ASG2_DUYTEA/src/Message/Message.java
@@ -0,0 +1,13 @@
+package Message;
+
+class Message {
+    private String content;
+
+    public Message(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
